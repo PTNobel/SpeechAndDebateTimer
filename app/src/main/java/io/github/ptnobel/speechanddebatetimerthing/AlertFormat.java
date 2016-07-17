a670343c7dd965f1dc3911853706ffcf509f91d8
Parth Nobel <pnob99@gmail.com>
2016-07-16 18:50:58 -0700

    Adds copyright.

--- app/src/main/java/io/github/ptnobel/speechanddebatetimerthing/AlertFormat.java
+++ app/src/main/java/io/github/ptnobel/speechanddebatetimerthing/AlertFormat.java
@@ -1,12 +1,32 @@
+/*
+
+    Copyright 2016 Parth Nobel
+
+    This file is part of Speech and Debate Timer.
+
+    Speech and Debate Timer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Speech and Debate Timer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Speech and Debate Timer.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
 package io.github.ptnobel.speechanddebatetimerthing;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.Toast;
 
 /**
  * Created by parth on 7/16/16.
@@ -103,7 +123,7 @@ public class AlertFormat {
         {
             Log.d("ElseBlock", "Inside the Else Block");
             startChronometer();
-            startStopButton.setText("Stop Timer");
+            startStopButton.setText("Pause Timer");
             resetButton.setText("Pause to Reset");
         }
     }
--- app/src/main/java/io/github/ptnobel/speechanddebatetimerthing/Timer.java
+++ app/src/main/java/io/github/ptnobel/speechanddebatetimerthing/Timer.java
@@ -1,10 +1,27 @@
+/*
+    Copyright 2016 Parth Nobel
+
+    This file is part of Speech and Debate Timer.
+
+    Speech and Debate Timer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Speech and Debate Timer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Speech and Debate Timer.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 package io.github.ptnobel.speechanddebatetimerthing;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
